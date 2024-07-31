5c1c71d6fa218cd7c1e9e16a2adff77dccc5b0b1
EC2 Default User <163057625+tomotomo11@users.noreply.github.com>
2024-07-31 04:13:08 +0000

    Add_book_model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'books/new'
+  get 'books/index'
+  get 'books/show'
+  get 'books/edit'
   devise_for :users
   root to: "homes#top"
   get "home/about" => "homes#about", as: "about"
