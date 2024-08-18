6b88f9058f76159a317e0c9cdf1de2d79b053df8
EC2 Default User <163057625+tomotomo11@users.noreply.github.com>
2024-08-18 08:06:44 +0000

    新規投稿機能追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,14 +1,13 @@
 class BooksController < ApplicationController
-  def new
-  end
-
   def index
+    @books = Book.all
     @book = Book.new
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
     redirect_to books_path
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
   root to: "homes#top"
+  devise_for :users
+  resources :books, only: [:index, :create, :show, :edit, :update, :destroy]
   get "home/about" => "homes#about", as: "about"
-  resources :books, only: [:new, :index, :create, :show, :edit, :update, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
